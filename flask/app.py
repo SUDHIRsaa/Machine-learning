8d7ec36d952c154f1ac9e042d253041577f69a92
SUDHIRsaa <140333096+SUDHIRsaa@users.noreply.github.com>
2024-07-20 11:51:38 +0530

    commit

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 # It creates an instance of the flask class,
 # which is the WSGI application.
 
@@ -7,12 +7,32 @@ app=Flask(__name__)
 @app.route('/')
 def welcome():
     return "<h1 >Welcome to the Homeeeesssee Page<h1>"
-@app.route('/index')
+@app.route('/index',methods=['GET'])
 def index():
     return render_template('index.html')
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name=request.form['name']
+        email=request.form['email']
+        password=request.form['password']
+        return f"<h1> Name:<i> {name} <i/> Email: <i>{email}<i/>  <i>Password: {password}<h1/>" 
+        
+    return render_template('form.html')
+@app.route('/submit',methods=['GET','POST'])
+def submit():
+    if request.method=='POST':
+        name=request.form['name']
+        email=request.form['email']
+        password=request.form['password']
+        return f"<h1> Name:<i> {name} <i/> Email: <i>{email}<i/>  <i>Password: {password}<h1/>" 
+        
+    return render_template('form.html')
+
 
 # the below line will run first in the entire code.
 if __name__ == '__main__':
